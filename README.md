bbdaec5c0ae47ad2dde7ebf890f0e9f4850627d5
manvendramaan <manvendra2215@gmail.com>
2019-09-08 18:44:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# GitHub Repo Search
+
+An easy to search user's repository on GitHub app using Vanilla JavaScript
+
+## Deployed -
+
+# To run the project:
+
+1. Clone the repository or download it
+2. Open index.html
---
+++ script.js
@@ -0,0 +1,91 @@
+
+let fetchRepo = debounce(fetchUserRepo, 300);
+function debounce(func, delay) { 
+    let debounceTimer; 
+    return function() { 
+        const context = this
+        const args = arguments 
+            clearTimeout(debounceTimer) 
+                debounceTimer 
+            = setTimeout(() => func.apply(context, args), delay) 
+    } 
+}  
+
+function resetfirst()
+{
+    document.getElementById('repos').innerHTML = '';
+    document.getElementById('input1').value = '';
+}
+
+function resetSecond()
+{
+    document.getElementById('repos').innerHTML = '';
+    document.getElementById('input2').value = '';
+}
+
+async function fetchUserRepo(repoInput)
+{
+    try{
+        const user = repoInput.value;
+        if(user === '')
+        {
+            resetfirst();
+            return;
+        }
+        const call = await fetch(`https://api.github.com/users/${user}/repos`);
+        const data = await call.json();
+        if(data.length === 0)
+        {
+            document.getElementById('repos').innerHTML = '';
+            let new_row = document.createElement('div');
+            new_row.className = "repoDiv";
+            let a = document.createElement("A");
+            let t = document.createTextNode('No Repository Found for given user');
+            a.appendChild(t);
+            new_row.appendChild(a);
+            document.getElementById('repos').appendChild(new_row);
+            return;
+        }
+        document.getElementById('repos').innerHTML = '';
+        for(let i=0; i< data.length; i++)
+        {
+            let new_row = document.createElement('div');
+            new_row.className = "repoDiv";
+            let a = document.createElement("A");
+            let t = document.createTextNode(data[i].name);
+            a.setAttribute("href", data[i].html_url);
+            a.appendChild(t);
+            new_row.appendChild(a);
+            document.getElementById('repos').appendChild(new_row);
+        }
+    }catch(e)
+    {
+        document.getElementById('repos').innerHTML = '';
+        let new_row = document.createElement('div');
+        new_row.className = "repoDiv";
+        let a = document.createElement("A");
+        let t = document.createTextNode('No Repository Found for given user');
+        a.appendChild(t);
+        new_row.appendChild(a);
+        document.getElementById('repos').appendChild(new_row);
+    }
+}
+
+function search() {
+    let input, filter, repos, repoDiv, a, i, txtValue;
+    input = document.getElementById("input2");
+    filter = input.value.toUpperCase();
+    repos = document.getElementById("repos");
+    repoDiv = repos.getElementsByClassName("repoDiv");
+    for (i = 0; i < repoDiv.length; i++) {
+      a = repoDiv[i].getElementsByTagName("a")[0];
+      if (a) {
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            repoDiv[i].style.display = "";
+        } else {
+            repoDiv[i].style.display = "none";
+        }
+      }       
+    }
+  }
